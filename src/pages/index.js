b5a15ae276a15e8bb95255338065869aab44be55
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-05-22 10:36:14 +0300

    added lorem ipsum

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,23 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Welcome to my blog</h1>
     <p>This is a sample blog site</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel
+      eros id purus ultrices dignissim sed non dolor. Curabitur vel laoreet
+      ante. Pellentesque vel accumsan elit. Maecenas dapibus, nulla vitae
+      laoreet sodales, turpis est tincidunt risus, vel tempor elit nisi vitae
+      mauris. Etiam convallis fermentum diam, eu vestibulum urna porta ut.
+      Aliquam metus ante, pharetra at luctus et, accumsan vitae nulla. Vivamus
+      suscipit mattis nisi volutpat pellentesque. Etiam eu tristique elit, sit
+      amet iaculis leo. Mauris eget aliquet risus. Nullam vitae ipsum magna.
+      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+      cubilia curae; Suspendisse varius augue ac sem porta facilisis. Praesent
+      dignissim dolor ut purus faucibus, eu lobortis augue commodo. Vivamus
+      ultrices ultrices orci a eleifend. Donec fermentum finibus nisi, sed
+      mattis lectus posuere eget. Aliquam vel accumsan ante. Vestibulum molestie
+      libero tortor, non elementum nisl egestas in. Aliquam posuere non sem eget
+      gravida.
+    </p>
   </Layout>
 )
 

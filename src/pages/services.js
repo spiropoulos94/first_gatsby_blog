b267b09ae53f531855628bfd923d8ba3cf243116
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-05-22 10:29:51 +0300

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -7,8 +7,11 @@ const ServicesPage = () => {
       <Layout>
         <h1>Our Services</h1>
         <p>
-          This is a sample blog siteThis is a sample blog siteThis is a sample
-          blog siteThis is a sample blog siteThis is a sample blog site
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel eros id purus ultrices dignissim sed non dolor. Curabitur vel laoreet ante. Pellentesque vel accumsan elit. Maecenas dapibus, nulla vitae laoreet sodales, turpis est tincidunt risus, vel tempor elit nisi vitae mauris. Etiam convallis fermentum diam, eu vestibulum urna porta ut. Aliquam metus ante, pharetra at luctus et, accumsan vitae nulla. Vivamus suscipit mattis nisi volutpat pellentesque. Etiam eu tristique elit, sit amet iaculis leo. Mauris eget aliquet risus. Nullam vitae ipsum magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse varius augue ac sem porta facilisis.
+
+Praesent dignissim dolor ut purus faucibus, eu lobortis augue commodo. Vivamus ultrices ultrices orci a eleifend. Donec fermentum finibus nisi, sed mattis lectus posuere eget. Aliquam vel accumsan ante. Vestibulum molestie libero tortor, non elementum nisl egestas in. Aliquam posuere non sem eget gravida. Suspendisse sit amet leo at nunc viverra mollis. Maecenas dapibus facilisis magna ut tempor. Phasellus viverra consectetur turpis ut venenatis. In quis bibendum turpis. Integer sed turpis lorem. Sed eleifend felis et porttitor lobortis. Sed scelerisque placerat metus.
+
+Sed non dictum enim, non aliquam arcu. Mauris blandit id quam in posuere. Pellentesque dapibus, lorem nec accumsan lobortis, nunc nulla faucibus dui, eu dignissim tortor eros eget velit. Duis eget libero ut lacus ornare fermentum. Aliquam lacinia dignissim nibh at dictum. Pellentesque tortor erat, dapibus quis turpis sit amet, vestibulum dapibus erat. Morbi pulvinar sapien quis ipsum iaculis, id iaculis nisi viverra. Praesent accumsan orci eu ornare vestibulum. Ut congue eu odio sed porta. Cras ac finibus nisi, et varius ex. Nunc tortor lectus, volutpat semper placerat quis, ornare eget erat.
         </p>
       </Layout>
     </div>
